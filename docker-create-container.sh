03210e6a0aba238c0a49ffa6a84ecd08d66bdc94
Bruno Perel <brunoperel@gmail.com>
2016-12-04 15:58:53 +0100

    Always restart the container

--- docker-create-container.sh
+++ docker-create-container.sh
@@ -7,7 +7,7 @@ HOST_PORT=$3
 . coa.properties
 
 docker rm -f $CONTAINER_NAME
-docker run --name $CONTAINER_NAME -p $HOST_PORT:3306 -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD -d $IMAGE_NAME
+docker run --restart always --name $CONTAINER_NAME -p $HOST_PORT:3306 -e MYSQL_ROOT_PASSWORD=$DB_PASSWORD -d $IMAGE_NAME
 if [ $? -eq 0 ]; then
     while [[ ! `docker logs $CONTAINER_NAME 2>&1 | grep 'Server socket created'` ]]; do echo -n "." && sleep 1; done
     echo -e "\nMySQL daemon started\n"
